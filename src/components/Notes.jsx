f02e2fcb58929ccd8b871cb13a1a77cad42cf798
Monick Marinho <monickmarinhoc@gmail.com>
2024-03-15 09:27:52 -0300

    home- update

---
+++ src/components/Notes.jsx
@@ -0,0 +1,9 @@
+export default function Notes() {
+    return (
+      <>
+        
+      </>
+    );
+}
+
+{/* <AddNote value={input} placeholder="Adicione uma nota..." onChange={(e) => setInput(e.target.value)} handleAdd={add} />  */}
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,18 +1,13 @@
 import Header from "../components/Header";
+import Notes from "../components/Notes";
 
 export default function Home() {
   return (
     <>
       <div className=" flex flex-col items-center justify-start gap-5 p-8 w-full min-h-screen">
         <Header />
+        <Notes />
       </div>
     </>
   );
 }
-
-{/* <section className="flex flex-row items-center justify-center gap-4 p-2 w-full">
-<Input value={input} placeholder="Adicione uma nota..." onChange={(e) => setInput(e.target.value)} />
-<button onClick={add} className="w-20 h-10 outline-none rounded p-1 text-base font-medium border-2"
-style={{ color: "var(--color)", borderColor: "var(--color)"}}
->Adicionar</button>
-</section>  */}
