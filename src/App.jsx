0a2a9365c50d45b467b4cebd94924629f9537414
Monick Marinho <monickmarinhoc@gmail.com>
2024-03-15 09:15:15 -0300

    app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./index.css"
+import Home from "./page/Home";
+
+export default function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -9,3 +9,10 @@ export default function Home() {
     </>
   );
 }
+
+{/* <section className="flex flex-row items-center justify-center gap-4 p-2 w-full">
+<Input value={input} placeholder="Adicione uma nota..." onChange={(e) => setInput(e.target.value)} />
+<button onClick={add} className="w-20 h-10 outline-none rounded p-1 text-base font-medium border-2"
+style={{ color: "var(--color)", borderColor: "var(--color)"}}
+>Adicionar</button>
+</section>  */}
