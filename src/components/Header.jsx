9f50685de103ffaa7e6210c7f99690f0cff19321
Monick Marinho <monickmarinhoc@gmail.com>
2024-03-15 09:05:45 -0300

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {  
+    return (
+      <>
+      <header className="w-full h-full flex flex-row flex-wrap justify-around items-center gap-6">         
+          <h1 className="text-5xl font-semibold" style={{ color: "var(--color)"}}>Notes</h1>
+      </header>
+      </>
+    )
+  }
---
+++ src/page/Home.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <>
+      <div className=" flex flex-col items-center justify-start gap-5 p-8 w-full min-h-screen">
+        <Header />
+      </div>
+    </>
+  );
+}
