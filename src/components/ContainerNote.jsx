5c173cefd64711e2a0f479767a2f7abf583b5ef3
Monick Marinho <139016074+monickevelyn@users.noreply.github.com>
2024-04-06 21:33:29 -0300

    --update container-note 1.2

--- src/components/ContainerNote.jsx
+++ src/components/ContainerNote.jsx
@@ -4,7 +4,7 @@ export default function ContainerNote({ text, onClick, id }) {
     <>
       <div
         id={id}
-        className="min-w-72 w-96 h-fit min-h-44 rounded font-normal text-1xl flex flex-col justify-start items-start gap-4 break-all overflow-y-auto"
+        className="min-w-72 w-96 h-fit min-h-36 max-h-96 rounded font-normal text-1xl flex flex-col justify-start items-start gap-4 break-all overflow-y-auto"
         style={{ backgroundColor: "var(--color-container)" }}
       >
         <div className="w-full p-2 self-end border-x-2" style={{ borderColor: "var(--color)"}} >
